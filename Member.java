869181a22cd2bbd5b705d8ea4e099e1341cdaa4a
RustamSalomov <250051@newuu.uz>
2026-05-06 20:56:53 +0500

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,8 +1,22 @@
 public class Member extends Account {
     private static final int MAX_BOOKS = 5;
 
+    private List<BookItem> borrowedBooks = new ArrayList<>();
+
     public Member(String id, String name) {
         super(id, name);
+    }
+
+    public void borrowBook(BookItem item) {
+        borrowedBooks.add(item);
+    }
+
+    public void returnBook(BookItem item) {
+        borrowedBooks.remove(item);
+    }
+
+    public int getBorrowedBooksCount() {
+        return borrowedBooks.size();
     }
 
     @Override
