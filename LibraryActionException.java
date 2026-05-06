f879cdb29f2cc1763ccf786975548e15043c4b8c
RustamSalomov <250051@newuu.uz>
2026-05-06 21:02:08 +0500

    Update LibraryActionException.java

--- LibraryActionException.java
+++ LibraryActionException.java
@@ -1,7 +1,29 @@
+public class LibraryActionHandler {
 
+    public void checkOutBook(Member member, BookItem item)
+            throws LibraryActionException {
 
-public class LibraryActionException extends Exception {
-    public LibraryActionException(String message) {
-        super(message);
+        if (item.getStatus() != BookStatus.AVAILABLE) {
+            throw new LibraryActionException("Book not available.");
+        }
+
+        if (member.getBorrowedBooksCount() >= member.getMaxBooksAllowed()) {
+            throw new LibraryActionException("Limit reached.");
+        }
+
+        item.checkout(member);
+        member.borrowBook(item);
     }
-}
+
+    public double returnBook(Member member, BookItem item)
+            throws LibraryActionException {
+
+        if (item.getStatus() != BookStatus.LOANED) {
+            throw new LibraryActionException("Book is not loaned.");
+        }
+
+        member.returnBook(item);
+        item.returnBook();
+
+        return 0;
+    }
