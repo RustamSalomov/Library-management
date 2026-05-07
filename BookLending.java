2fa9fec67dd205f065cdb0409621537cdf355bdf
RustamSalomov <250051@newuu.uz>
2026-05-07 22:14:03 +0500

    Update BookLending.java

--- BookLending.java
+++ BookLending.java
@@ -1,33 +1,32 @@
-import java.util.*;
+import java.time.LocalDate;
 
 public class BookLending {
-    private Date creationDate;
-    private Date dueDate;
-    private Date returnDate;
 
-    private BookItem bookItem;
-    private Account borrower;
+    private String bookBarcode;
+    private String memberId;
 
-    public BookLending(BookItem bookItem, Account borrower) {
-        this.bookItem = bookItem;
-        this.borrower = borrower;
-        this.creationDate = new Date();
+    private LocalDate creationDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
 
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, 10);
-        this.dueDate = cal.getTime();
+    public BookLending(String bookBarcode, String memberId) {
+
+        this.bookBarcode = bookBarcode;
+        this.memberId = memberId;
+
+        this.creationDate = LocalDate.now();
+        this.dueDate = creationDate.plusDays(10);
     }
 
-    public double returnBook() {
-        this.returnDate = new Date();
-        bookItem.returnBook();
+    public String getBookBarcode() {
+        return bookBarcode;
+    }
 
-        long diff = returnDate.getTime() - dueDate.getTime();
-        long daysLate = diff / (1000 * 60 * 60 * 24);
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
 
-        if (daysLate > 0) {
-            return daysLate * 1.0; 
-        }
-        return 0;
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
     }
 }
