fffdcb59bb16703773f960956b13d99bc7b3b96a
RustamSalomov <250051@newuu.uz>
2026-05-07 22:16:58 +0500

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class Member extends Account {
+
     private static final int MAX_BOOKS = 5;
 
     private List<BookItem> borrowedBooks = new ArrayList<>();
+    private List<BookLending> lendings = new ArrayList<>();
+    private List<Fine> fines = new ArrayList<>();
 
     public Member(String id, String name) {
         super(id, name);
@@ -17,6 +22,38 @@ public class Member extends Account {
 
     public int getBorrowedBooksCount() {
         return borrowedBooks.size();
+    }
+
+    public int getTotalBooksCheckedOut() {
+        return borrowedBooks.size();
+    }
+
+    public void addLending(BookLending lending) {
+        lendings.add(lending);
+    }
+
+    public void removeLending(BookLending lending) {
+        lendings.remove(lending);
+    }
+
+    public BookLending getLendingForBook(String barcode) {
+
+        for (BookLending lending : lendings) {
+
+            if (lending.getBookBarcode().equals(barcode)) {
+                return lending;
+            }
+        }
+
+        return null;
+    }
+
+    public void addFine(Fine fine) {
+        fines.add(fine);
+    }
+
+    public String getId() {
+        return id;
     }
 
     @Override
